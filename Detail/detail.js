53aa5314f331db8e848df879c1cbd2df0951f832
yiseul <nisuri@knou.ac.kr>
2025-03-05 23:24:06 +0900

    식물 정보 수정 버튼 추가, 물주기 변경 드롭다운 추가

--- Detail/detail.js
+++ Detail/detail.js
@@ -9,3 +9,27 @@ function readURL(input) {
       document.getElementById('preview').src = "";
     }
   }
+
+  // 🚨 1. DB에서 식물정보 뿌려주는 로직 
+
+  // 식물 정보 수정 및 저장
+  document.querySelectorAll(".edit-btn").forEach((button)=>{
+    button.addEventListener("click",(event)=>{
+      const targetId = event.currentTarget.dataset.target;
+      const targetElement = document.getElementById(targetId);
+
+      // 눌렀을때 수정가능하게
+      targetElement.contentEditable = "true";
+      targetElement.focus();
+      targetElement.style.border = "2px solid #4CAF50";
+      targetElement.style.borderRadius = "10px"
+
+      // 🚨 3. DB에 저장하는 함수 호출 
+    })
+  })
+  
+  // 🚨 2. DB에 저장하는 함수
+  
+  
+  
+// 물 주기 섹션
