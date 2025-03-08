659e13d35f731bed7ee5474e774d7d74eae2c49c
dayeDev <dazam1@naver.com>
2025-03-08 19:31:29 +0900

    식물피드 링크삭제 / alt수정

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-// 1️⃣ 세션 값을 강제로 설정 (임시 로그인, 로그아웃 상태에서는 실행되지 않도록 수정)
-// if (!sessionStorage.getItem("plantsSessionNumOne") && sessionStorage.getItem("forceLogout") !== "true") {
-//   sessionStorage.setItem("plantsSessionNumOne", "testUser123");
-// }
-
 const sessionValue = sessionStorage.getItem("plantsSessionNumOne");
 const loginButton = document.getElementById("index-login");
 const memberName = document.getElementById("index-member-name");
@@ -11,11 +6,6 @@ const indexMyPlantsSection = document.getElementById("index-my-plants-section");
 const indexFeed = document.querySelector("#index-feed");
 
 if (sessionValue !== null) {
-  // 2️⃣ 가짜 데이터 생성 (디비 없이 테스트용)
-  // const fakeData = [{ id: "testUser123", name: "김다예" }];
-
-  // 3️⃣ `sessionValue`와 일치하는 멤버 찾기
-  // const member = fakeData.find((member) => member.id === sessionValue);
 
   fetch('https://silk-scandalous-boa.glitch.me/members')
       .then(response => response.json())
@@ -42,7 +32,6 @@ if (sessionValue !== null) {
   // 로그아웃 기능
   loginButton.addEventListener("click", () => {
     sessionStorage.removeItem("plantsSessionNumOne"); // 로그인 정보 삭제
-    // sessionStorage.setItem("forceLogout", "true"); // 강제 로그아웃 상태 설정
     window.location.href = "/Login/login.html";
   });
   
@@ -80,7 +69,7 @@ for (let i = 0; i < 10; i++) {
         <a class="index-feed-plant">
           <img
           src="https://search.pstatic.net/sunny/?src=https%3A%2F%2Fcdn-icons-png.flaticon.com%2F512%2F6098%2F6098665.png&type=a340"
-          alt="내 식물"
+          alt="식물 피드"
           />
         </a>`;
     document.querySelector("#index-feed").append(feedItem);
